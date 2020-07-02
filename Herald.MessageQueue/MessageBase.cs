119099b178d36077ed0c43603500560d82f537d1
tcfialho <tcfialho@gmail.com>
2020-07-02 00:01:52 -0300

    BREAKING CHANGE: removing CreationDate from MessageBase

--- Herald.MessageQueue/MessageBase.cs
+++ Herald.MessageQueue/MessageBase.cs
@@ -6,14 +6,7 @@ namespace Herald.MessageQueue
 {
     public abstract class MessageBase
     {
-        public MessageBase()
-        {
-            CreationDate = DateTime.UtcNow;
-        }
-
-        public object QueueData { get; set; }
-
-        [JsonProperty]
-        public DateTime CreationDate { get; private set; }
+        [JsonIgnore]
+        public virtual object QueueData { get; set; }
     }
 }
